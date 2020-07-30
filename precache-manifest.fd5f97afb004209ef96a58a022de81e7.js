562c1ad27842081bcc85a7a8c77814f66f2cdcdd
Nikhil Bagal <nbagal02@gmail.com>
2020-07-31 00:29:10 +0530

    Updates

---
+++ precache-manifest.fd5f97afb004209ef96a58a022de81e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3ba395f3774286cfbc183f5399c0923",
+    "url": "/admin_dashboard/index.html"
+  },
+  {
+    "revision": "a0f1b6f2a7042ed7b6a3",
+    "url": "/admin_dashboard/static/css/main.d7e46c07.chunk.css"
+  },
+  {
+    "revision": "5ad7c64053105a8607ab",
+    "url": "/admin_dashboard/static/js/2.408b0418.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/admin_dashboard/static/js/2.408b0418.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0f1b6f2a7042ed7b6a3",
+    "url": "/admin_dashboard/static/js/main.21f06dc1.chunk.js"
+  },
+  {
+    "revision": "67902fc14f4956700810",
+    "url": "/admin_dashboard/static/js/runtime-main.1217e99e.js"
+  }
+]);
